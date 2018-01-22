bf4c6febdb7ec126aa6acd6511c950bc5b705eb0
Sairagul Abdukhalieva <abdukhalieva@gmail.com>
2018-01-21 19:08:50 -0800

    Added Remaining days for the goal

--- app/src/main/java/com/akzholus/easydiet/valuetypes/GoalTextCardDataVT.java
+++ app/src/main/java/com/akzholus/easydiet/valuetypes/GoalTextCardDataVT.java
@@ -1,11 +1,21 @@
 package com.akzholus.easydiet.valuetypes;
 
+import android.util.Log;
+
 import com.akzholus.easydiet.common.Constants;
 import com.akzholus.easydiet.common.Formatters;
+import com.akzholus.easydiet.common.GoalProcessPolicy;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public final class GoalTextCardDataVT {
+    private static final String TAG = "";
     private final Date startDate;
     private final Date endDate;
     private final int remainingDurationInDay;
@@ -29,16 +39,31 @@ public final class GoalTextCardDataVT {
         this.pledgeAmount = pledgeAmount;
     }
 
-    public static GoalTextCardDataVT fromGoal(GoalVT goal) {
+    public static GoalTextCardDataVT fromGoal(GoalVT goal) throws ParseException {
+
+        DateTime endDate = new DateTime(goal.getEndDate());
+        int remainingDurationInDays = Days.daysBetween(DateTime.now().toLocalDate(), endDate.toLocalDate()).getDays() + 1;
+
+        Log.d(Constants.TAG, "Conversion: " + goal.getGoalId() + " " + remainingDurationInDays + " => " + endDate.toString() + " " + goal.getEndDate() + " " + DateTime.now().toString());
+
         return new GoalTextCardDataVT(goal.getStartDate(), goal.getEndDate(),
-                24, // enddate - today
+                remainingDurationInDays,
                 goal.getGoalPoundsPerWeek(),
                 Constants.GOAL_LENGTH_IN_WEEKS,
-                0.4f, // goal.getActualWeights() - currentWeight
+                0.4f, // goal.getActualWeights() - WeightInVT weightInVT
                 goal.getRefereeEmailAddress(),
                 goal.getPledgeAmount());
+
+
     }
 
+    public DateTime getStartDate() {
+        return new DateTime((startDate));
+    }
+
+    public DateTime getEndDate() {
+        return getStartDate().plus(GoalProcessPolicy.getDurationBetweenCheckins().multipliedBy(getGoalDurationInWeeks()));
+    }
 
     public String getFormattedStartDate() {
         return Formatters.formatGoalDateTime(startDate);
@@ -84,4 +109,5 @@ public final class GoalTextCardDataVT {
     public float getPledgeAmount() {
         return pledgeAmount;
     }
+
 }
